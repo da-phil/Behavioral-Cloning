0eeb114bae676a91ada6027e11eebf191cf4ab6c
Philipp Lutz <philipp.lutz@gmx.de>
2017-10-31 02:17:26 +0100

    implemented generator and commaAI model in training.py

--- training.py
+++ training.py
@@ -15,33 +15,71 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D, Lambda, Cropping2D
+import sklearn
 from sklearn.model_selection import train_test_split
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 
 
 
 
-def LeNet3_model(keep_prob=0.5):
-    # really make sure it gets the dimensions right!
-
+def commaAI_model(keep_prob=0.5):
     model = Sequential()
     model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
     model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(3,160,320)))
-    model.add(Conv2D(64, kernel_size=(3, 3), input_shape=(3,90,320), activation="elu"))
-    model.add(MaxPooling2D((3,3)))
-    model.add(Conv2D(128, (3, 3), activation="elu"))
-    model.add(MaxPooling2D((3, 3)))
+    model.add(Conv2D(16, kernel_size=(3, 3), strides=(4,4), input_shape=(3,90,320), activation="elu"))
+    model.add(Conv2D(32, kernel_size=(3, 3), strides=(2,2), activation="elu"))
     model.add((Dropout(keep_prob)))
+    model.add(Conv2D(64, kernel_size=(3, 3), strides=(2,2), activation="elu"))
     model.add(Flatten())
-    model.add(Dense(128, activation="elu"))
+    model.add(Dense(512, activation="elu"))
     model.add(Dropout(keep_prob))
     model.add(Dense(1))
-    model.compile(loss="mse", optimizer=keras.optimizers.Adadelta()) #, metrics=["accuracy"])
+    model.compile(loss="mse", optimizer="adam")
     return model
     
 
+def nvidia_model(keep_prob=0.5):
+    model = Sequential()
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+    model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(3,160,320)))
+    model.add(Conv2D(16, kernel_size=(3, 3), strides=(4,4), input_shape=(3,90,320), activation="elu"))
+    model.add(Conv2D(32, kernel_size=(3, 3), strides=(2,2), activation="elu"))
+    model.add((Dropout(keep_prob)))
+    model.add(Conv2D(64, kernel_size=(3, 3), strides=(2,2), activation="elu"))
+    model.add(Flatten())
+    model.add(Dense(512, activation="elu"))
+    model.add(Dropout(keep_prob))
+    model.add(Dense(1))
+    model.compile(loss="mse", optimizer="adam")
+    return model
+
+
+
 def process_image(img):
     return img.astype("float32")
+
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                filename = batch_sample[0].split("/")[-1]
+                current_path = training_set + "/IMG/" + filename
+                center_image = np.asarray(Image.open(current_path))
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
 
 
 
@@ -76,39 +114,31 @@ if __name__ == "__main__":
         help="Batchsize for training"
     )
     parser.add_argument(
-        "--weights_file",
+        "--model",
         type=str,
         nargs="?",
-        default="LeNet3_weights.h5",
-        help="File used to store weight values of model"
-    )
-    parser.add_argument(
-        "--model_file",
-        type=str,
-        nargs="?",
-        default="LeNet3_model.h5",
-        help="Batchsize for training"
+        default="commaAI",
+        help="Choose between models: commaAI, nvidia"
     )
     
     args = parser.parse_args()
-    steering_correction = 0.2
-    lenet3_weights  = args.weights_file
-    lenet3_model    = args.model_file
-    nr_training_run = args.trainingset
-    epochs          = args.epochs
-    BATCH_SIZE      = args.batchsize
     
+    weights_file    = args.model + "-weights.h5"
+    model_file      = args.model + "-model.h5"
+
     # Parse csv file
-    training_set  = "../run%02d" % nr_training_run
+    training_set  = "../run%02d" % args.trainingset
     training_data = {"center_imgs": [], "left_imgs": [], "right_imgs": [],
                     "steering_angle_center": [], "steering_angle_left": [], "steering_angle_right": [],
                     "throttle": [], "brake": [], "speed": []}
-    training_samples = 0
+    samples = []
     with open(training_set+"/driving_log.csv") as logfile:
         reader = csv.reader(logfile, delimiter=",")
         header = next(reader) # it"s safe to skip first row in case it contains headings
+        
         for line in reader:
-            training_samples += 1
+            samples.append(line)
+            """
             # images
             for i in range(3):
                 filename = line[i].split("/")[-1]
@@ -123,13 +153,14 @@ if __name__ == "__main__":
                     training_data["left_imgs"].append(img)
                 if i == 2:
                     training_data["right_imgs"].append(img)
+            """
             # car control signals
             for i in range(3,7):
                 if i == 3:
                     # create adjusted steering measurements for the side camera images
                     training_data["steering_angle_center"].append(float(line[i]))
-                    training_data["steering_angle_left"].append(float(line[i]) + steering_correction)
-                    training_data["steering_angle_right"].append(float(line[i]) - steering_correction)
+                    training_data["steering_angle_left"].append(float(line[i]) + args.steering_corr)
+                    training_data["steering_angle_right"].append(float(line[i]) - args.steering_corr)
                 if i == 4:
                     training_data["throttle"].append(line[i])
                 if i == 5:
@@ -137,50 +168,72 @@ if __name__ == "__main__":
                 if i == 6:
                     training_data["speed"].append(line[i])
 
-    """
-    # preprocessing
-    for i in range(training_samples):
-        training_data["center_imgs"][i] = np.fliplr(training_data["center_imgs"][i])
-        training_data["left_imgs"][i]   = np.fliplr(training_data["left_imgs"][i])
-        training_data["right_imgs"][i]  = np.fliplr(training_data["right_imgs"][i])
-        training_data["steering"][i]    = -training_data["steering_angle_center"]
-    """
-    
-    try:
-        LeNet3 = load_model(lenet3_model)
-        print("Loaded pretrained LeNet3 keras model!")
-    except:        
-        LeNet3 = LeNet3_model(0.2)
-        try:
-            LeNet3.load_weights(lenet3_weights)
-            print("Loaded weights for LeNet3 keras model!")
-        except:
-            print("Could not load weights for LeNet3 keras model, starting training from scratch!")
 
+
+    try:
+        model = load_model(model_file)
+        print("Loaded pretrained %s keras model!" % args.model)
+    except:        
+        if args.model == "commaAI":
+            model = commaAI_model(0.2)
+        elif args.model == "nvidia":
+            model = nvidia_model(0.2)
+        else:
+            print("Model '%s' not known!" % args.model)
+            sys.exit(-1)
+        try:
+            model.load_weights(weights_file)
+            print("Loaded weights for %s keras model!" % args.model)
+        except:
+            print("Could not load weights for %s keras model from file %s!\nStarting training from scratch!" %
+                    (args.model, weights_file))
 
     # define callbacks for the checkpoint saving and early stopping
     callbacks_list = [
-        EarlyStopping(monitor="loss", patience=2, verbose=1),
-        ModelCheckpoint(lenet3_weights, monitor="loss", save_best_only=True, mode="auto", save_weights_only=True, verbose=1)
+        EarlyStopping(monitor="loss", patience=3, verbose=1),
+        ModelCheckpoint(weights_file, monitor="loss", save_best_only=True, mode="auto", save_weights_only=True, verbose=1)
     ]
 
+    ### plot the training and validation loss for each epoch
+    fig = plt.figure()
+    plt.title("run%02d visualization" % args.trainingset)
+    ax1 = plt.subplot(2,1,1)
+    ax1.plot(training_data["steering_angle_center"])
+    ax1.plot(training_data["throttle"])
+    ax1.plot(training_data["brake"])
+    ax1.legend(["steering", "throttle", "brake"])
+    plt.grid()
+    ax2 = plt.subplot(2,1,2, sharex=ax1)
+    ax2.plot(training_data["speed"])
+    ax2.legend(["speed"])
+    plt.grid(), plt.show()
+    
+    
+    train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+    nr_train_samples = len(train_samples)
+    nr_valid_samples = len(validation_samples)
+    print("Samples in training set:   %d" % nr_train_samples)
+    print("Samples in validation set: %d" % nr_valid_samples)
+    
     biased_index = []
     #biased_index.append(class_indexes_train[30][0])
     #biased_index.append(class_indexes_train[27][0])
     #biased_index = np.array(list(itertools.chain.from_iterable(biased_index)))
 
-    sample_selection        = range(training_samples) #balanced_index #biased_index
-    images_train            = np.array(training_data["center_imgs"])[sample_selection]
-    steering_angles_train   = np.array(training_data["steering_angle_center"])[sample_selection]
-
-    # get rid of them for now (does not work anyway? garbage collector doesn't seem to clean up...)    
-    del training_data["center_imgs"]
-    del training_data["left_imgs"]
-    del training_data["right_imgs"]
-
-    LeNet3.summary()
-    model_history = LeNet3.fit(images_train, steering_angles_train, validation_split=0.2, shuffle=True,
-                                batch_size=BATCH_SIZE, epochs=epochs, verbose=1, callbacks=callbacks_list)
+    sample_selection        = range(nr_train_samples) #balanced_index #biased_index
+    #images_train            = np.array(training_data["center_imgs"])[sample_selection]
+    #steering_angles_train   = np.array(training_data["steering_angle_center"])[sample_selection]
+    
+    # compile and train the model using the generator function
+    train_generator = generator(train_samples, batch_size=args.batchsize)
+    validation_generator = generator(validation_samples, batch_size=args.batchsize)
+    
+    model.summary()
+    #model_history = model.fit(images_train, steering_angles_train, validation_split=0.2, shuffle=True,
+    #                            batch_size=args.batchsize, epochs=args.epochs, verbose=1, callbacks=callbacks_list)
+    model_history = model.fit_generator(train_generator, steps_per_epoch=nr_train_samples // args.batchsize, epochs=args.epochs,
+                                         validation_data=validation_generator, validation_steps=nr_valid_samples // args.batchsize,
+                                         callbacks=callbacks_list, verbose=1)
 
     ### plot the training and validation loss for each epoch
     plt.plot(model_history.history['loss'])
@@ -191,5 +244,5 @@ if __name__ == "__main__":
     plt.legend(['training set', 'validation set'], loc='upper right')
     plt.show()
 
-    LeNet3.save_weights(lenet3_weights)
-    LeNet3.save(lenet3_model)
+    model.save_weights(weights_file)
+    model.save(model_file)

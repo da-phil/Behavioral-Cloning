9aa4fe63f8adee93ae38dfa3ba5715858af9d970
Philipp Lutz <philipp.lutz@gmx.de>
2017-11-10 17:54:51 +0100

    fixed nvidia model and added image explaining the steering model by using 3 cameras.

--- model.py
+++ model.py
@@ -48,9 +48,9 @@ def nvidia_model(keep_prob=0.5):
     model.add(Conv2D(64, kernel_size=(3, 3), strides=(1,1), activation="elu"))
     model.add((Dropout(keep_prob)))
     model.add(Flatten())
-    model.add(Dense(1164, activation="elu"))
     model.add(Dropout(keep_prob))
     model.add(Dense(100, activation="elu"))
+    model.add(Dense(50, activation="elu"))
     model.add(Dense(10, activation="elu"))
     model.add(Dense(1))
     return model
